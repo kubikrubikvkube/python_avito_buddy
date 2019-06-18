7b7dd89fb4ba56a311bb24138b76d64d79322b4b
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-06-18 18:17:01 +0300

    Not implemented yet

--- avito_russia/avito_russia/spiders/russia.py
+++ avito_russia/avito_russia/spiders/russia.py
@@ -1,40 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
-import operator
-from datetime import datetime, timedelta
-from typing import Optional
 
 import scrapy
-
-
-class RussiaSpiderHelper:
-    current_link: str = None
-    processed: dict = {}
-
-    @classmethod
-    def add_processed(cls, id, timestamp) -> None:
-        cls.processed[id] = timestamp
-
-    @classmethod
-    def is_processed(cls, id) -> bool:
-        return id in cls.processed.keys()
-
-    @classmethod
-    def processed_timestamp(cls, id) -> Optional[int]:
-        return cls.processed[id]
-
-    @classmethod
-    def generate_url(cls) -> str:
-        if not cls.processed:
-            delta_timestamp = datetime.now() - timedelta(minutes=1)
-            timestamp: float = int(datetime.timestamp(delta_timestamp))
-            print(f'Initial timestamp is {timestamp}')
-            return f'https://m.avito.ru/api/9/items?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&sort=default&locationId=621540&page=1&lastStamp={timestamp}&display=list&limit=30'
-        else:
-            sorted_dict = sorted(cls.processed.items(), key=operator.itemgetter(1))
-            latest_timestamp = sorted_dict[0][1]
-            print(f'Sorted dict value 1 is {latest_timestamp}')
-            return f'https://m.avito.ru/api/9/items?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&sort=default&locationId=621540&page=1&lastStamp={latest_timestamp}&display=list&limit=30'
 
 
 class AvitoSimpleAd(scrapy.Item):
@@ -68,7 +35,7 @@ class RussiaSpider(scrapy.Spider):
 
     allowed_domains = ['m.avito.ru']
     offset = 30
-    start_urls = [RussiaSpiderHelper.generate_url()]
+    start_urls = [None]
 
     def start_requests(self):
         return super().start_requests()
@@ -77,14 +44,7 @@ class RussiaSpider(scrapy.Spider):
         json_response = json.loads(response.body_as_unicode())
         items = json_response['result']['items']
         for item in items:
-            if item['type'] == 'vip' or RussiaSpiderHelper.is_processed(item['value']['id']):
-                pass
-            elif item['type'] != 'vip' and not RussiaSpiderHelper.is_processed(item['value']['id']):
-                id = item['value']['id']
-                timestamp = item['value']['time']
-                RussiaSpiderHelper.add_processed(id, timestamp)
             yield AvitoSimpleAd(item['value'])
-
-        next_page_url = RussiaSpiderHelper.generate_url()
+        next_page_url = None
         if items:
             yield scrapy.Request(response.urljoin(next_page_url))
