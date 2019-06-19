fb732b856224a486ceebabe151b0aa445970524b
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-06-19 20:01:15 +0300

    Middle state commit pagination 2

--- avito_russia/avito_russia/spiders/russia.py
+++ avito_russia/avito_russia/spiders/russia.py
@@ -1,7 +1,45 @@
 # -*- coding: utf-8 -*-
 import json
+from datetime import datetime, timedelta
 
 import scrapy
+
+
+class Paginator:
+    url_pattern = 'https://m.avito.ru/api/9/items?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&sort=date&locationId=621540&page={page}&lastStamp={timestamp}&display=list&limit=99'
+
+    def __init__(self) -> None:
+        delta_timestamp = datetime.now() - timedelta(minutes=3)
+        self.last_stamp = int(datetime.timestamp(delta_timestamp))
+        self.page = 1
+        self.uris = []
+
+    def preserve(self, ad_json) -> None:
+        uri = None
+        timestamp = None
+        if ad_json['type'] == 'item':
+            uri = ad_json['value']['uri']
+            timestamp = ad_json['value']['time']
+        elif ad_json['type'] == 'vip':
+            uri = ad_json['value']['list'][0]['value']['uri']
+            timestamp = ad_json['value']['list'][0]['value']['time']
+
+        if uri in self.uris:
+            print(f'Last stamp is {self.last_stamp}')
+            self.last_stamp = timestamp
+            print(f'Page is {self.page}')
+            self.page = 0
+        else:
+            print('Not in self.uris')
+            print(f'Appending uri {uri}')
+            self.uris.append(uri)
+            self.page += 1
+            print(f'Page is {self.page}')
+
+    def next_url(self) -> str:
+        page = str(self.page)
+        last_stamp = str(self.last_stamp)
+        return self.url_pattern.replace('{page}', page).replace('{timestamp}', last_stamp)
 
 
 class AvitoSimpleAd(scrapy.Item):
@@ -35,16 +73,23 @@ class RussiaSpider(scrapy.Spider):
 
     allowed_domains = ['m.avito.ru']
     offset = 30
-    start_urls = [None]
+
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.paginator = Paginator()
 
     def start_requests(self):
+        self.start_urls = []
+        self.start_urls.append(self.paginator.next_url())
         return super().start_requests()
 
     def parse(self, response):
+        print(response)
         json_response = json.loads(response.body_as_unicode())
         items = json_response['result']['items']
         for item in items:
+            self.paginator.preserve(item)
             yield AvitoSimpleAd(item['value'])
-        next_page_url = None
+        next_url = self.paginator.next_url()
         if items:
-            yield scrapy.Request(response.urljoin(next_page_url))
+            yield scrapy.Request(response.urljoin(next_url))
