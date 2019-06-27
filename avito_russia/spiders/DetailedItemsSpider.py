7ccbc6141d9e1507d216a77e8683d18b2d8f9e6b
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-06-27 23:51:16 +0300

    Can connecto to postgresql

--- avito_russia/spiders/DetailedItemsSpider.py
+++ avito_russia/spiders/DetailedItemsSpider.py
@@ -1,14 +1,19 @@
 import logging
 
+import psycopg2
 import scrapy
+from psycopg2._psycopg import connection
 
-from avito_russia.settings import API_KEY
+from avito_russia.settings import API_KEY, POSTGRES_DBNAME, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST
 
+logger = logging.getLogger("DetailedItemsSpider")
+logger.setLevel(level=logging.DEBUG)
 
 class DetailedItemsSpider(scrapy.Spider):
     name = 'detailed'
     allowed_domains = ['m.avito.ru']
     url_pattern = f"https://m.avito.ru/api/13/items/__id__?key={API_KEY}&action=view"
+    connection: connection
 
     custom_settings = {
         'ITEM_PIPELINES': {
@@ -16,9 +21,21 @@ class DetailedItemsSpider(scrapy.Spider):
         }
     }
 
+    def __init__(self, name=None, **kwargs):
+        logging.info(f"DetailedItemsSpider initialized")
+        logger.info("Trying to establish PostgreSQL connection")
+        self.connection = psycopg2.connect(dbname=POSTGRES_DBNAME, user=POSTGRES_USER,
+                                           password=POSTGRES_PASSWORD, host=POSTGRES_HOST)
+        with self.connection.cursor() as cursor:
+            cursor.execute(f"SELECT to_regclass('{POSTGRES_DBNAME}');")
+            is_exists = cursor.fetchone()[0] is not None
+            logger.info(f"Table {POSTGRES_DBNAME} exists {is_exists}")
+            assert is_exists
+        logger.info("PostgreSQL DB connection opened")
+        super().__init__(name, **kwargs)
+
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
-        logging.info(f"DetailedItemsSpider initialized")
         return super().from_crawler(crawler, *args, **kwargs)
 
     def start_requests(self):
@@ -33,5 +50,9 @@ class DetailedItemsSpider(scrapy.Spider):
 
     @staticmethod
     def close(spider, reason):
+        spider.connection.close()
+        is_closed = spider.connection.closed
+        logger.info(f"PostgreSQL connection is closed {bool(is_closed)}")
+        assert is_closed
         logging.info(f"DetailedItemsSpider closed: {reason}")
         return super().close(spider, reason)
