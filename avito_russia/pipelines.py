3c8542d4294c336e2881edbda255fcbf4bf13ab1
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-06-27 22:45:40 +0300

    New database format

--- avito_russia/pipelines.py
+++ avito_russia/pipelines.py
@@ -28,7 +28,6 @@ class DatabaseSavingPipeline(ABC):
     Abstract class for avito saving pipelines to inherit from
     """
 
-
     @abstractmethod
     def process_item(self, ad: AvitoSimpleAd, spider) -> AvitoSimpleAd:
         """
@@ -40,20 +39,8 @@ class DatabaseSavingPipeline(ABC):
 
     def convert_ad_item_to_list(self, ad: AvitoSimpleAd) -> List:
         assert ad['id'] is not None
-        id = int(ad['id'])
-        category_id = int(ad['category']['id']) if 'category' in ad else None
-        category_name = str(ad['category']['name']) if 'category' in ad else None
-        location = str(ad['location']) if 'location' in ad else None
-        coords_lat = float(ad['coords']['lat']) if 'coords' in ad else None
-        coords_lng = float(ad['coords']['lng']) if 'coords' in ad else None
-        time = int(ad['time']) if 'time' in ad else None
-        title = str(ad['title']) if 'title' in ad else None
-        userType = str(ad['userType']) if 'userType' in ad else None
-        price = str(ad['price']) if 'price' in ad else None
-        uri_mweb = str(ad['uri_mweb']) if 'uri_mweb' in ad else None
-
-        return [id, category_id, category_name, location, coords_lat, coords_lng, time, title, userType, price,
-                uri_mweb]
+        assert ad['time'] is not None
+        return [int(ad['id']), int(ad['time'])]
 
 
 class SQLiteSavingPipeline(DatabaseSavingPipeline):
@@ -70,7 +57,7 @@ class SQLiteSavingPipeline(DatabaseSavingPipeline):
         :return:
         """
         list = self.convert_ad_item_to_list(ad)
-        self.connection.execute("INSERT INTO avito_simple_ads VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", list)
+        self.connection.execute("INSERT INTO avito_simple_ads VALUES (?,?)", list)
         self.connection.commit()
         spider.processed_items += 1
         sqlite_logger.info(f'Processed {spider.processed_items} items')
@@ -85,24 +72,8 @@ class SQLiteSavingPipeline(DatabaseSavingPipeline):
 
         self.connection = sqlite3.connect('avito_russia.db')
         sqlite_logger.info("SQLiteSavingPipeline DB connection opened")
-        self.connection.execute('''CREATE TABLE IF NOT EXISTS avito_simple_ads
-                                                    (id integer,
-                                                    category_id integer,
-                                                    category_name text,
-                                                    location text,
-                                                    coords_lat real,
-                                                    coords_lng real,
-                                                    time integer,
-                                                    title text,
-                                                    userType text,
-                                                    images text,
-                                                    services text,
-                                                    price text,
-                                                    uri text,
-                                                    uri_mweb text,
-                                                    isVerified text,
-                                                    isFavorite text)''')
-
+        self.connection.execute(
+            '''CREATE TABLE IF NOT EXISTS avito_simple_ads(id integer,time integer,is_processed text)''')
 
     def close_spider(self, spider) -> None:
         """
@@ -126,12 +97,12 @@ class PostgreSQLSavingPipeline(DatabaseSavingPipeline):
                 postgresql_logger.info(f'This ad already indexed and saved to DB {ad}')
                 spider.dropped_items += 1
                 spider.dropped_items_in_a_row += 1
-                print(
-                    f"DropItem, dropped items {spider.dropped_items}, dropped items in a row {spider.dropped_items_in_a_row}")
+                msg = f"DropItem, dropped items {spider.dropped_items}, dropped items in a row {spider.dropped_items_in_a_row}"
+                print(msg)
                 raise DropItem()
             else:
                 list = self.convert_ad_item_to_list(ad)
-                request = f"INSERT INTO {POSTGRES_DBNAME} VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                request = f"INSERT INTO {POSTGRES_DBNAME} VALUES (%s,%s,false)"
                 cursor.execute(request, list)
                 self.connection.commit()
                 spider.processed_items += 1
@@ -163,16 +134,8 @@ class PostgreSQLSavingPipeline(DatabaseSavingPipeline):
             if not is_table_exists:
                 cursor.execute("CREATE TABLE IF NOT EXISTS {}"
                                "(id bigint NOT NULL,"
-                               "category_id integer,"
-                               "category_name text,"
-                               "location text,"
-                               "coords_lat float8,"
-                               "coords_lng float8,"
                                "time bigint,"
-                               "title text,"
-                               "userType text,"
-                               "price text,"
-                               "uri_mweb text,"
+                               "is_detailed bool,"
                                "PRIMARY KEY (id)"
                                ")".format(POSTGRES_DBNAME))
                 is_exists_after = self._is_table_exists(POSTGRES_DBNAME)
@@ -180,6 +143,7 @@ class PostgreSQLSavingPipeline(DatabaseSavingPipeline):
                 info_msg = f"Is '{POSTGRES_DBNAME}' table exists after CREATE TABLE execution - {is_exists_after}"
                 cursor.execute(f"CREATE UNIQUE INDEX idx_id ON {POSTGRES_DBNAME}(id);")
                 cursor.execute(f"CREATE INDEX idx_time ON {POSTGRES_DBNAME}(time);")
+                cursor.execute(f"CREATE INDEX idx_is_detailed ON {POSTGRES_DBNAME}(is_detailed);")
                 postgresql_logger.debug(info_msg)
             self.connection.commit()
 
