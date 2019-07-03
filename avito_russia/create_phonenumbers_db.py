852df8d04a2019e6876452465b6246dfc7f2f75b
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-07-03 13:57:33 +0300

    Async

--- avito_russia/create_phonenumbers_db.py
+++ avito_russia/create_phonenumbers_db.py
@@ -3,6 +3,7 @@ import logging
 import sqlite3
 import urllib
 from collections import namedtuple
+from multiprocessing.pool import Pool
 from typing import Set, Optional
 from urllib.parse import urlparse, parse_qsl
 
@@ -20,16 +21,25 @@ logging.basicConfig(
     ])
 
 
-def find_all_is_detailed_items(cursor) -> Set[int]:
-    cursor.execute(f"SELECT id FROM {POSTGRES_DBNAME} WHERE is_detailed = True ORDER BY random()")
-    ids = set()
-    for raw_id in cursor:
-        id = raw_id[0]
-        ids.add(id)
-    return ids
+def find_all_is_detailed_items(postgresql_connection) -> Set[int]:
+    """
+    Resolving item status  - it is stored in PostreSQL, but is it already processed and stored in MongoDB?
+    :param postgresql_connection:
+    :return:
+    """
+    with conn.cursor() as cursor:
+        cursor.execute(f"SELECT id FROM {POSTGRES_DBNAME} WHERE is_detailed = True ORDER BY random()")
+        ids = set()
+        for raw_id in cursor:
+            id = raw_id[0]
+            ids.add(id)
+        return ids
 
 
 class NamesDatabase:
+    """
+    Russian names database
+    """
     def __init__(self) -> None:
         self.conn = sqlite3.connect(':memory:')
         logging.info("SQLite3 in-memory names database initialized")
@@ -44,7 +54,12 @@ class NamesDatabase:
         logging.info("SQLite3 in-memory names database loaded")
 
     def resolve_gender(self, name: str) -> Optional[str]:
-        cursor = self.conn.execute("""SELECT gender FROM names WHERE name LIKE ? LIMIT 1""", [name])
+        """
+        Resolving gender from name
+        :param name: name
+        :return: gender
+        """
+        cursor = self.conn.execute("""SELECT gender FROM names WHERE name LIKE ? LIMIT 1""", [name.capitalize()])
         result = cursor.fetchone()
         if result:
             return result[0]
@@ -56,8 +71,11 @@ class NamesDatabase:
         logging.info("SQLite3 in-memory names database closed")
 
 
+def strip(string_to_strip: str) -> str:
+    return str.strip(string_to_strip)
+
 if __name__ == '__main__':
-    n = NamesDatabase()
+    names_db = NamesDatabase()
     with psycopg2.connect(dbname=POSTGRES_DBNAME, user=POSTGRES_USER, password=POSTGRES_PASSWORD,
                           host=POSTGRES_HOST) as conn:
         logging.info("PostgreSQLSavingPipeline DB connection opened")
@@ -66,10 +84,9 @@ if __name__ == '__main__':
         detailed_collection = avito_db["detailed"]
         logging.info(f"MongoDB connection info: {client.server_info()}")
         with open('phone_numbers.csv', mode='w', newline='', encoding='utf-8') as phonenumbers_file:
-            phonenumber_writer = csv.writer(phonenumbers_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            with conn.cursor() as postgresql_cursor:
-                ids = find_all_is_detailed_items(postgresql_cursor)
-                for id in ids:
+            pool = Pool(processes=10)
+            for id in find_all_is_detailed_items(conn):
+                def process_id(id):
                     for item in detailed_collection.find({"_id": id}):
                         if item['adjustParams']['vertical'] == "JOB":
                             # резюме
@@ -82,18 +99,24 @@ if __name__ == '__main__':
                             phone_number = None
                             Row = namedtuple('Row', 'name gender postfix address phone_number')
                             try:
-                                name = str.strip(item['seller']['name'])
-                                gender = n.resolve_gender(name)
-                                postfix = str.strip(item['seller']['postfix'])
-                                address = str.strip(item['address'])
+                                name = strip(item['seller']['name'])
+                                gender = names_db.resolve_gender(name)
+                                postfix = strip(item['seller']['postfix'])
+                                address = strip(item['address'])
                                 raw_phone = item['contacts']['list'][0]['value']['uri']
                                 r = urllib.parse.unquote_plus(raw_phone)
                                 q2 = parse_qsl(urlparse(r).query)
-                                phone_number = str.strip(q2[0][1])
+                                phone_number = strip(q2[0][1])
                             except (KeyError, IndexError) as e:  # why indexerror? no phone supplied?
                                 pass
                             row = Row(name, gender, postfix, address, phone_number)
-                            print(row)
-                            phonenumber_writer.writerow([name, gender, postfix, address, phone_number])
+                            # print(row)
+                            writer = csv.writer(phonenumbers_file, delimiter=',', quotechar='"',
+                                                quoting=csv.QUOTE_MINIMAL)
+                            writer.writerow([name, gender, postfix, address, phone_number])
+
+
+                pool.apply_async(process_id(id))
+
 
         client.close()
