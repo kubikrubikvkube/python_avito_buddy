9a3502433cdc0e1634ec4961ef8d7331440e215f
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-07-03 14:52:36 +0300

    Optimization applied

--- avito_russia/create_phonenumbers_db.py
+++ avito_russia/create_phonenumbers_db.py
@@ -2,7 +2,6 @@ import csv
 import logging
 import sqlite3
 import urllib
-from collections import namedtuple
 from multiprocessing.pool import Pool
 from typing import Set, Optional
 from urllib.parse import urlparse, parse_qsl
@@ -68,7 +67,9 @@ class NamesDatabase:
 
     def __del__(self):
         self.conn.close()
-        logging.info("SQLite3 in-memory names database closed")
+
+
+#        logging.info("SQLite3 in-memory names database closed")
 
 
 def strip(string_to_strip: str) -> str:
@@ -85,36 +86,37 @@ if __name__ == '__main__':
         logging.info(f"MongoDB connection info: {client.server_info()}")
         with open('phone_numbers.csv', mode='w', newline='', encoding='utf-8') as phonenumbers_file:
             pool = Pool(processes=10)
-            for id in find_all_is_detailed_items(conn):
+            items = find_all_is_detailed_items(conn)
+            print(len(items))
+            for id in items:
                 def process_id(id):
-                    for item in detailed_collection.find({"_id": id}):
-                        if item['adjustParams']['vertical'] == "JOB":
-                            # резюме
+                    item = detailed_collection.find_one(id)
+                    if item is None or item['adjustParams']['vertical'] == "JOB":
+                        # резюме или None
+                        pass
+                    else:
+                        name = None
+                        gender = None
+                        postfix = None
+                        address = None
+                        phone_number = None
+                        # Row = namedtuple('Row', 'name gender postfix address phone_number')
+                        try:
+                            name = strip(item['seller']['name'])
+                            gender = names_db.resolve_gender(name)
+                            postfix = strip(item['seller']['postfix'])
+                            address = strip(item['address'])
+                            raw_phone = item['contacts']['list'][0]['value']['uri']
+                            r = urllib.parse.unquote_plus(raw_phone)
+                            q2 = parse_qsl(urlparse(r).query)
+                            phone_number = strip(q2[0][1])
+                        except (KeyError, IndexError) as e:  # why indexerror? no phone supplied?
                             pass
-                        else:
-                            name = None
-                            gender = None
-                            postfix = None
-                            address = None
-                            phone_number = None
-                            Row = namedtuple('Row', 'name gender postfix address phone_number')
-                            try:
-                                name = strip(item['seller']['name'])
-                                gender = names_db.resolve_gender(name)
-                                postfix = strip(item['seller']['postfix'])
-                                address = strip(item['address'])
-                                raw_phone = item['contacts']['list'][0]['value']['uri']
-                                r = urllib.parse.unquote_plus(raw_phone)
-                                q2 = parse_qsl(urlparse(r).query)
-                                phone_number = strip(q2[0][1])
-                            except (KeyError, IndexError) as e:  # why indexerror? no phone supplied?
-                                pass
-                            row = Row(name, gender, postfix, address, phone_number)
-                            # print(row)
-                            writer = csv.writer(phonenumbers_file, delimiter=',', quotechar='"',
-                                                quoting=csv.QUOTE_MINIMAL)
-                            writer.writerow([name, gender, postfix, address, phone_number])
-
+                        # row = Row(name, gender, postfix, address, phone_number)
+                        # print(row)
+                        writer = csv.writer(phonenumbers_file, delimiter=',', quotechar='"',
+                                            quoting=csv.QUOTE_MINIMAL)
+                        writer.writerow([name, gender, postfix, address, phone_number])
 
                 pool.apply_async(process_id(id))
 
