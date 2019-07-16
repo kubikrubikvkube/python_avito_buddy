f2d55236211c179367a5e11ea96feccb4e55ea74
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-07-16 15:38:09 +0300

    Bugfix - collectionName was not customizable

--- avito_russia/mongodb.py
+++ avito_russia/mongodb.py
@@ -8,7 +8,7 @@ class MongoDB:
     def __init__(self, collection_name: str) -> None:
         self.client = client = pymongo.MongoClient()
         self.db = db = client["avito"]
-        self.collection = collection = db["detailed"]
+        self.collection = collection = db[collection_name]
         logging.info("MongoDB db_connection opened")
         logging.info(f"MongoDB server info: {client.server_info()}")
 
