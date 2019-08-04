fa635cd17faa845e384718a7b794978b2f810ba9
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-08-04 17:07:58 +0300

    Fixed

--- avito_russia/run.py
+++ avito_russia/run.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
 import logging
-
-from scrapy.crawler import CrawlerRunner
+from time import sleep
+from datetime import datetime
+from scrapy.crawler import CrawlerRunner, CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from twisted.internet import reactor
 from twisted.internet import task, defer
@@ -14,20 +15,21 @@ if __name__ == '__main__':
     logger = logging.getLogger("RecentSpider")
     logger.setLevel(level=logging.INFO)
 
-
     logging.info('CrawlerProcess initialized')
     locations = LocationManager().locations_list.keys()
 
+    runner = CrawlerProcess(get_project_settings())
 
-    def crawl_both():
-        runner = CrawlerRunner(get_project_settings())
+
+    def crawl():
+        logging.info(f"Sleeping 1 minute at {datetime.now()} - just in case")
+        sleep(60)
         for location in locations:
             runner.crawl(RecentSpider, location_name=location)
             runner.crawl(DetailedItemsSpider, location_name=location)
         d: defer = runner.join()
-        d.addBoth(lambda _: logging.info("Deferred returned that crawling is finished"))
+        d.addBoth(lambda _: crawl())
 
 
-    l = task.LoopingCall(crawl_both)
-    l.start(28800)  # call every 8 hours
-    reactor.run()
+    crawl()
+    runner.start(False)
