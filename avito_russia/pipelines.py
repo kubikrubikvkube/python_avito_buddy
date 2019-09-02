ac0c9476795185dadf501e9fe43061847f634547
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-09-02 12:25:03 +0300

    New format impelented

--- avito_russia/pipelines.py
+++ avito_russia/pipelines.py
@@ -15,55 +15,117 @@ class DetailedItemSaverPipeline:
     def __init__(self) -> None:
         logging.info("DetailedItemSaverPipeline initialised")
 
-    def process_item(self, item: DetailedItem, spider: AvitoSpider):
+    def process_item(self, raw_item: DetailedItem, spider: AvitoSpider):
         if not isinstance(spider, DetailedItemsSpider):
             logging.debug("DetailedItemSaverPipeline method called not from DetailedItemsSpider")
-        elif "seller" not in item:
-            logging.debug(f"DetailedItem {item} is not advertisement for sale. Skipping this item")
-            pass
+        elif "seller" not in raw_item:
+            logging.debug(f"DetailedItem {raw_item} is not advertisement for sale. Skipping this item")
         else:
-            #Resolve gender
-            if item['seller']['name']:
+            processed_item = {}
+
+            # 0. Prepare nested dicts
+            processed_item['seller'] = {}
+            processed_item['sharing'] = {}
+            processed_item['price'] = {}
+
+            # 1. Preserve categoryId
+            processed_item['categoryId'] = raw_item['categoryId']
+
+            # 2. Preserve anonymousNumber
+            if 'anonymousNumber' in raw_item:
+                processed_item['anonymousNumber'] = raw_item['anonymousNumber']
+
+            # 3. Preserve description
+            if 'description' in raw_item:
+                processed_item['description'] = raw_item['description']
+
+            # 4. Resolve gender
+            if 'seller' in raw_item and 'name' in raw_item['seller']:
                 name_request_json = {
-                    "name": item['seller']['name']
+                    "name": raw_item['seller']['name']
                 }
                 r = requests.post(GENDER_RESOLVER_HOST, json=name_request_json)
                 if r.status_code == 200:
                     gender = r.json()['gender']
                     if gender != "UNKNOWN":
-                        item['gender'] = gender.lower()
+                        processed_item['gender'] = gender.lower()
 
-            #Mark UUID
-            item['uuid'] = str(uuid.uuid4())
+            # 5. Resolve coordinates
+            processed_item['location'] = {
+                "type": "Point",
+                "coordinates": [
+                    raw_item['coords']['lng'],
+                    raw_item['coords']['lat']
+                ]
+            }
 
-            #Store prices as ints
-            if item['price']['value']:
-                invalid_price_values = ["Цена не указана", "Договорная", "Бесплатно"]
-                price = item['price']['value']
-                if not invalid_price_values.count(price):
-                    item['price']['value'] = int(price.replace(" ", ""))
+            # 6. Preserve location
+            if 'metroId' in raw_item:
+                processed_item['metroId'] = raw_item['metroId']
 
-            #Decode phonenumber
-            decoded_phone_number = DetailedItem.decode_phone_number(item)
+            # 7. Preserve advertisement URL
+            if 'sharing' in raw_item and 'url' in raw_item['sharing']:
+                processed_item['sharing']['url'] = raw_item['sharing']['url']
+
+            # 8. Preserve parameters
+            processed_item['parameters'] = raw_item['parameters']
+
+            # 9. Decode phonenumber
+            decoded_phone_number = DetailedItem.decode_phone_number(raw_item)
             if decoded_phone_number and PhoneNumberValidator.is_valid(decoded_phone_number):
-                item['phonenumber'] = decoded_phone_number
-            #Set coordinates
-            if item['coords']:
-                item['location'] = {
-                        "type": "Point",
-                        "coordinates": [
-                            item['coords']['lng'],
-                            item['coords']['lat']
-                        ]
-                }
+                processed_item['phonenumber'] = decoded_phone_number
 
-            #Decode userKey
-            if item['userType'] == 'private' and item['seller']['link']:
-                r = urllib.parse.unquote_plus(item['seller']['link'])
+            # 10. Preserve seller manager name
+            if 'seller' in raw_item and 'manager' in raw_item['seller']:
+                processed_item['seller']['manager'] = raw_item['seller']['manager']
+
+            # 11. Preserve shopId
+            if 'shopId' in raw_item:
+                processed_item['shopId'] = raw_item['shopId']
+
+            # 12. Resolve price
+            invalid_price_values = ["Цена не указана", "Договорная", "Бесплатно"]
+            price = raw_item['price']['value']
+            if not invalid_price_values.count(price):
+                processed_item['price']['value'] = int(price.replace(" ", ""))
+
+            # 13. Preserve seller name
+            if 'seller' in raw_item and 'name' in raw_item['seller']:
+                processed_item['seller']['name'] = raw_item['seller']['name']
+
+            # 14. Preserve userHashId
+            if raw_item['seller'] and raw_item['seller']['userHashId']:
+                processed_item['seller']['userHashId'] = raw_item['seller']['userHashId']
+
+            # 15. Preserve seller userkey
+            if 'seller' in raw_item and 'userKey' in raw_item['seller']:
+                processed_item['seller']['userKey'] = raw_item['seller']['userKey']
+
+            # 16. Preserve timestamp
+            processed_item['time'] = raw_item['time']
+
+            # 17. Preserve title
+            processed_item['title'] = raw_item['title']
+
+            # 18. Preserve description
+            if 'description' in raw_item:
+                processed_item['description'] = raw_item['description']
+
+            # 19. Preserve userType
+            processed_item['userType'] = raw_item['userType']
+
+            # 20. Resolve UUID
+            processed_item['uuid'] = str(uuid.uuid4())
+
+            # 21. Mark as 'new'
+            processed_item['newFormat'] = True
+
+            # 22. Resolve userKey
+            if raw_item['userType'] == 'private' and raw_item['seller']['link']:
+                r = urllib.parse.unquote_plus(raw_item['seller']['link'])
                 q2 = parse_qsl(urlparse(r).query)
                 userKey = str(q2[0][1]).strip()
-                item['seller']['userKey'] = userKey
+                processed_item['seller']['userKey'] = userKey
 
-            item_json = dict(item)
-            logging.debug(f"Processing {item_json}")
-            spider.detailed_collection.insert_one(item_json)
+            logging.debug(f"Processing {processed_item}")
+            spider.detailed_collection.insert_one(processed_item)
